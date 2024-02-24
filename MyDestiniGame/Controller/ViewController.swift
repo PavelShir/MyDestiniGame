7cde3e6f36e7a86d13f5410cddeeb9a288cd003f
Pavel Shirokiy <pshirokiy@gmail.com>
2024-02-24 16:25:01 +0300

    all done, didnt commit

--- MyDestiniGame/Controller/ViewController.swift
+++ MyDestiniGame/Controller/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var storyBrain = StoryBrain()
+    
+    @IBOutlet var storyLabel: UILabel!
+    @IBOutlet var choice1Button: UIButton!
+    @IBOutlet var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateTitle()
+        
     }
 
+    @IBAction func choiceButtonPress(_ sender: UIButton) {
+    
+        let answer = sender.currentTitle!
+        
+        if storyBrain.checkAnswer(answer) {
+            sender.backgroundColor = .systemGreen
+        } else {
+            sender.backgroundColor = .systemRed
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateTitle), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc private func updateTitle() {
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1Text(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2Text(), for: .normal)
+        
+    }
 
 }
 
